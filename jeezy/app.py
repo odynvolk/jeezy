43f10331ec3d7b063f3f2a86b160381faebc810f
Alexi Rahman <alexi.rahman@r76.se>
2025-03-30 19:01:10 +0200

    More stuff

--- jeezy/app.py
+++ jeezy/app.py
@@ -26,28 +26,32 @@ class Jeezy(object):
 
     async def run(self):
         print("Run")
-        for x in range(3):
-          user_agent = random.sample(self.user_agents, 1)[0]
-          print(user_agent)
-          try:
-              # browser_args = [f'--user-agent={user_agent}', f'--proxy-server={proxy}', "--headless"]
-              browser_args = [f'--user-agent={user_agent}', "--headless"]
-              driver = await uc.start(browser_args=browser_args)
-              tab = await driver.get("https://www.youtube.com/watch?v=HX4abVLOeX4")
-              await driver.sleep(7)
-              await tab.scroll_down(200)
-              await driver.sleep(1)
-              reject = await tab.find("Reject all", best_match=True)
-              await driver.sleep(2)
-              await tab.scroll_down(100)
-              await reject.mouse_click()
-              await tab.scroll_down(100)
-              await driver.sleep(1300)
-              driver.stop()
+        for x in range(5):
+            user_agent = random.sample(self.user_agents, 1)[0]
+            url = random.sample(self.urls, 1)[0]
+            try:
+                # browser_args = [f'--user-agent={user_agent}', f'--proxy-server={proxy}', "--headless"]
+                browser_args = [f'--user-agent={user_agent}', "--headless"]
+                driver = await uc.start(browser_args=browser_args)
+                tab = await driver.get(url)
+                await driver.sleep(7)
+                await tab.scroll_down(200)
+                await driver.sleep(1)
+                reject = await tab.find("Reject all", best_match=True)
+                await driver.sleep(2)
+                await tab.scroll_down(100)
+                await reject.mouse_click()
+                await driver.sleep(2)
+                await tab.scroll_up(300)
+                await driver.sleep(1300)
+                driver.stop()
 
-              time.sleep(30 * 1000)
-          except Exception as e:
-            print(e)
+                time.sleep(120 * 60)
+            except Exception as e:
+              print(e)
+
+        print("Done!")
+
 
 if __name__ == "__main__":
     import traceback
